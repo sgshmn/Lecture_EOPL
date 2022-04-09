34ea5fe5b6023bb5a11b709c1022b22d0eb9b528
Kwanghoon Choi <lazyswamp@gmail.com>
2022-04-09 18:45:53 +0900

    ch4 implicitreflang

---
+++ ch4/app/implicitrefslang/Interp.hs
@@ -0,0 +1,85 @@
+module Interp where
+
+import Expr
+import EnvStore
+
+--
+value_of :: Exp -> Env -> Store -> (ExpVal, Store)   -- Sec 4.2.1: Store-passing specifications
+
+value_of (Const_Exp n) env store = (Num_Val n,store)
+
+value_of (Var_Exp var) env store = 
+  let (loc,store1) = apply_env env store var 
+      val = deref store1 loc
+  in  (val, store1)
+
+value_of (Diff_Exp exp1 exp2) env store =
+  let (val1,store1) = value_of exp1 env store
+      (val2,store2) = value_of exp2 env store1
+
+      num1 = expval_num val1
+      num2 = expval_num val2
+  in  (Num_Val (num1 - num2), store2)
+  
+value_of (IsZero_Exp exp) env store =
+  let (val1,store1) = value_of exp env store in
+    let num1 = expval_num val1 in
+      if num1 == 0
+      then (Bool_Val True,store1)
+      else (Bool_Val False,store1)
+
+value_of (If_Exp exp1 exp2 exp3) env store =
+  let (val1,store1) = value_of exp1 env store in
+    if expval_bool val1
+    then value_of exp2 env store1
+    else value_of exp3 env store1
+
+value_of (Let_Exp var exp1 body) env store =
+  let (val1,store1) = value_of exp1 env store 
+      (loc,store2)  = newref store1 val1
+  in
+    value_of body (extend_env var loc env) store2
+
+value_of (Letrec_Exp letbindings letrec_body) env store =
+  value_of letrec_body (extend_env_rec (extend letbindings) env) store
+  where extend [] = []
+        extend ((proc_name, bound_var, proc_body):letbindings) =
+          (proc_name,bound_var,proc_body) : extend letbindings
+
+value_of (Proc_Exp var body) env store =
+  (Proc_Val (procedure var body env),store)
+
+value_of (Call_Exp rator rand) env store =
+  let (val1,store1) = value_of rator env store
+      (val2,store2) = value_of rand env store1
+      
+      proc = expval_proc val1
+      arg  = val2
+  in apply_procedure proc arg store2
+
+value_of (Block_Exp [exp]) env store = value_of exp env store
+
+value_of (Block_Exp (exp:expList)) env store =
+  let (_,store1) = value_of exp env store
+  in value_of (Block_Exp expList) env store1
+
+value_of (Set_Exp x exp) env store =
+  let (val1,store1) = value_of exp env store
+      (loc,store2) = apply_env env store x
+  in  (Num_Val 23,setref store2 loc val1)       -- The dummy value, 23, comes from the EOPL book. :)
+
+--
+value_of_program :: Exp -> ExpVal
+
+value_of_program exp =
+  let (v,_) = value_of exp initEnv initStore in v
+
+
+--
+initEnv = empty_env
+
+--
+apply_procedure :: Proc -> ExpVal -> Store -> (ExpVal,Store)
+apply_procedure proc arg store =
+  let (loc,store1) = newref store arg in
+   value_of (body proc) (extend_env (var proc) loc (saved_env proc)) store1
--- ch4/app/implicitrefslang/Parser.hs
+++ ch4/app/implicitrefslang/Parser.hs
@@ -43,12 +43,6 @@ parserSpec = ParserSpec
       rule "Expression -> letrec LetRecBindings in Expression"
         (\rhs -> return $ fromExp $ Letrec_Exp (idIdExpListFrom (get rhs 2)) (expFrom (get rhs 4))),
 
-      rule "LetRecBindings -> identifier ( identifier ) = Expression"
-        (\rhs -> return $ fromIdIdExpList [(getText rhs 1, getText rhs 3, expFrom (get rhs 6))]),
-
-      rule "LetRecBindings -> identifier ( identifier ) = Expression LetRecBindings"
-        (\rhs -> return $ fromIdIdExpList ((getText rhs 1, getText rhs 3, expFrom (get rhs 6)) : idIdExpListFrom (get rhs 7))),
-
       rule "Expression -> proc ( identifier ) Expression"
         (\rhs -> return $ fromExp $ Proc_Exp (getText rhs 3) (expFrom (get rhs 5))),
 
@@ -58,27 +52,27 @@ parserSpec = ParserSpec
       rule "Expression -> begin ExpressionList end"
         (\rhs -> return $ fromExp $ Block_Exp (expListFrom (get rhs 2))),
 
+      rule "Expression -> set identifier = Expression"
+        (\rhs -> return $ fromExp $ Set_Exp (getText rhs 2) (expFrom (get rhs 4))),
+
+      rule "LetRecBindings -> identifier ( identifier ) = Expression"
+        (\rhs -> return $ fromIdIdExpList [(getText rhs 1, getText rhs 3, expFrom (get rhs 6))]),
+
+      rule "LetRecBindings -> identifier ( identifier ) = Expression LetRecBindings"
+        (\rhs -> return $ fromIdIdExpList ((getText rhs 1, getText rhs 3, expFrom (get rhs 6)) : idIdExpListFrom (get rhs 7))),
+
       rule "ExpressionList -> Expression"
         (\rhs -> return $ fromExpList $ [ expFrom (get rhs 1) ]),
 
       rule "ExpressionList -> Expression ; ExpressionList"
-        (\rhs -> return $ fromExpList $ (expFrom (get rhs 1) : expListFrom (get rhs 3))),
-
-      rule "Expression -> newref ( Expression )"
-        (\rhs -> return $ fromExp $ Newref_Exp (expFrom (get rhs 3))),
-
-      rule "Expression -> deref ( Expression )"
-        (\rhs -> return $ fromExp $ Deref_Exp (expFrom (get rhs 3))),
-
-      rule "Expression -> setref ( Expression , Expression )"
-        (\rhs -> return $ fromExp $ Setref_Exp (expFrom (get rhs 3)) (expFrom (get rhs 5)))
+        (\rhs -> return $ fromExpList $ (expFrom (get rhs 1) : expListFrom (get rhs 3)))        
     ],
     
     baseDir        = "./",
-    actionTblFile  = "action_table_explicitrefslang.txt",
-    gotoTblFile    = "goto_table_explicitrefslang.txt",
-    grammarFile    = "prod_rules_explicitrefslang.txt",
-    parserSpecFile = "mygrammar_explicitrefslang.grm",
+    actionTblFile  = "action_table_implicitrefslang.txt",
+    gotoTblFile    = "goto_table_implicitrefslang.txt",
+    grammarFile    = "prod_rules_implicitrefslang.txt",
+    parserSpecFile = "mygrammar_implicitrefslang.grm",
     genparserexe   = "yapb-exe"
   }
 
