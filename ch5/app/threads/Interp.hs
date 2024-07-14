5d6a29058b77f585d80296c99faeef0351cd7b46
Kwanghoon Choi <lazyswamp@gmail.com>
2024-07-14 10:46:36 +0900

    put cont after env in value_of

--- ch5/app/threads/Interp.hs
+++ ch5/app/threads/Interp.hs
@@ -55,15 +55,15 @@ apply_cont store sched cont val =
 
     apply_cont' store sched (Let_Exp_Cont var body env cont) val1 =
       let (loc,store') = newref store val1
-      in  value_of_k body (extend_env var loc env) store' sched cont
+      in  value_of_k body (extend_env var loc env) cont store' sched
 
     apply_cont' store sched (If_Test_Cont exp2 exp3 env cont) v =
       if expval_bool v
-      then value_of_k exp2 env store sched cont
-      else value_of_k exp3 env store sched cont
+      then value_of_k exp2 env cont store sched
+      else value_of_k exp3 env cont store sched
 
     apply_cont' store sched (Diff1_Cont exp2 env cont) val1 =
-      value_of_k exp2 env store sched (Diff2_Cont val1 cont)
+      value_of_k exp2 env (Diff2_Cont val1 cont) store sched
 
     apply_cont' store sched (Diff2_Cont val1 cont) val2 =
       let num1 = expval_num val1
@@ -75,7 +75,7 @@ apply_cont store sched cont val =
         res `seq` apply_cont store sched cont res
 
     apply_cont' store sched (Rator_Cont rand env cont) ratorVal =
-      value_of_k rand env store sched (Rand_Cont ratorVal cont)
+      value_of_k rand env (Rand_Cont ratorVal cont) store sched 
 
     apply_cont' store sched (Rand_Cont ratorVal cont) randVal =
       let proc = expval_proc ratorVal in
@@ -119,77 +119,80 @@ apply_unop Cdr (List_Val (_:xs)) = List_Val xs
 apply_unop Print v = trace (show v) $ List_Val []  -- ???
 
 --
-value_of_k :: Exp -> Env -> Store -> SchedState -> Cont -> (FinalAnswer, Store)
+value_of_k :: Exp -> Env -> Cont -> Store -> SchedState -> (FinalAnswer, Store)
 
-value_of_k (Const_Exp n) env store sched cont =
+value_of_k (Const_Exp n) env cont store sched =
   apply_cont store sched cont (Num_Val n)
 
-value_of_k (Const_List_Exp nums) env store sched cont =
+value_of_k (Const_List_Exp nums) env cont store sched =
   apply_cont store sched cont (List_Val (map Num_Val nums))
 
-value_of_k (Var_Exp var) env store sched cont =
+value_of_k (Var_Exp var) env cont store sched =
   let (loc,store') = apply_env env store var
       val = deref store' loc
   in
     apply_cont store' sched cont val
 
-value_of_k (Diff_Exp exp1 exp2) env store sched cont =
-  value_of_k exp1 env store sched (Diff1_Cont exp2 env cont)
+value_of_k (Diff_Exp exp1 exp2) env cont store sched =
+  value_of_k exp1 env (Diff1_Cont exp2 env cont) store sched 
 
-value_of_k (Unary_Exp op exp1) env store sched cont =
-  value_of_k exp1 env store sched (Unop_Arg_Cont op cont)
+value_of_k (Unary_Exp op exp1) env cont store sched =
+  value_of_k exp1 env (Unop_Arg_Cont op cont) store sched
   
-value_of_k (If_Exp exp1 exp2 exp3) env store sched cont =
-  value_of_k exp1 env store sched (If_Test_Cont exp2 exp3 env cont)
+value_of_k (If_Exp exp1 exp2 exp3) env cont store sched =
+  value_of_k exp1 env (If_Test_Cont exp2 exp3 env cont) store sched
 
-value_of_k (Let_Exp var exp1 body) env store sched cont =
-  value_of_k exp1 env store sched (Let_Exp_Cont var body env cont)
+value_of_k (Let_Exp var exp1 body) env cont store sched =
+  value_of_k exp1 env (Let_Exp_Cont var body env cont) store sched
 
-value_of_k (Letrec_Exp nameArgBodyList letrec_body) env store sched cont =
-  value_of_k letrec_body (extend_env_rec nameArgBodyList env) store sched cont
+value_of_k (Letrec_Exp nameArgBodyList letrec_body) env cont store sched =
+  value_of_k letrec_body (extend_env_rec nameArgBodyList env) cont store sched
 
-value_of_k (Proc_Exp var body) env store sched cont =
+value_of_k (Proc_Exp var body) env cont store sched =
   apply_cont store sched cont (Proc_Val (procedure var body env))
 
-value_of_k (Call_Exp rator rand) env store sched cont =
-  value_of_k rator env store sched (Rator_Cont rand env cont)
+value_of_k (Call_Exp rator rand) env cont store sched =
+  value_of_k rator env (Rator_Cont rand env cont) store sched
   
-value_of_k (Block_Exp [exp]) env store sched cont =
-  value_of_k exp env store sched cont 
+value_of_k (Block_Exp [exp]) env cont store sched =
+  value_of_k exp env cont store sched 
 
-value_of_k (Block_Exp (exp:exps)) env store sched cont =
-  value_of_k (Call_Exp (Proc_Exp "$dummy" (Block_Exp exps)) exp) env store sched cont
+value_of_k (Block_Exp (exp:exps)) env cont store sched =
+  value_of_k (Call_Exp (Proc_Exp "$dummy" (Block_Exp exps)) exp) env cont store sched
 
-value_of_k (Set_Exp x exp) env store sched cont =
+value_of_k (Set_Exp x exp) env cont store sched=
   let (loc,store') = apply_env env store x in
-  value_of_k exp env store' sched (Set_Rhs_Cont loc cont)
+  value_of_k exp env (Set_Rhs_Cont loc cont) store' sched 
 
-value_of_k (Spawn_Exp exp) env store sched cont =
-  value_of_k exp env store sched (Spawn_Cont cont)
+value_of_k (Spawn_Exp exp) env cont store sched =
+  value_of_k exp env (Spawn_Cont cont) store sched
 
-value_of_k (Yield_Exp) env store sched cont =
+value_of_k (Yield_Exp) env cont store sched =
   let yieldsched =
         place_on_ready_queue
           (\store' sched' -> apply_cont store' sched' cont (Num_Val 99))
           sched
   in  run_next_thread store yieldsched 
 
-value_of_k (Mutex_Exp) env store sched cont =
+value_of_k (Mutex_Exp) env cont store sched =
   let (mutex, store') = new_mutex store in
     apply_cont store' sched cont (Mutex_Val mutex)
 
-value_of_k (Wait_Exp exp) env store sched cont =
-  value_of_k exp env store sched (Wait_Cont cont)
+value_of_k (Wait_Exp exp) env cont store sched =
+  value_of_k exp env (Wait_Cont cont) store sched
 
-value_of_k (Signal_Exp exp) env store sched cont =
-  value_of_k exp env store sched (Signal_Cont cont)
+value_of_k (Signal_Exp exp) env cont store sched =
+  value_of_k exp env (Signal_Cont cont) store sched
 
 
 --
 value_of_program :: Exp -> Integer -> ExpVal
 
 value_of_program exp timeslice =
-  fst $ value_of_k exp initEnv initStore (initialize_scheduler timeslice) End_Main_Thread_Cont
+  let (finalVal, _) = 
+        value_of_k exp initEnv End_Main_Thread_Cont 
+           initStore (initialize_scheduler timeslice)
+  in finalVal
 
 
 --
@@ -199,4 +202,4 @@ initEnv = empty_env
 apply_procedure_k :: Proc -> ExpVal -> Store -> SchedState -> Cont -> (FinalAnswer, Store)
 apply_procedure_k proc arg store sched cont =
   let (loc,store') = newref store arg in
-   value_of_k (body proc) (extend_env (var proc) loc (saved_env proc)) store' sched cont
+   value_of_k (body proc) (extend_env (var proc) loc (saved_env proc)) cont store' sched
