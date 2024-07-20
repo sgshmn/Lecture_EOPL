0b1c2bc45cbb38c4622e8d4548d82ca7b25c8712
Kwanghoon Choi <lazyswamp@gmail.com>
2024-07-20 15:45:03 +0900

    module environments

--- ch8/app/simplemodules/Env.hs
+++ ch8/app/simplemodules/Env.hs
@@ -7,6 +7,7 @@ data Env =
     Empty_env
   | Extend_env Identifier ExpVal Env
   | Extend_env_rec Identifier Identifier Exp Env
+  | Extend_env_with_module Identifier TypedModule Env
 
 empty_env :: Env
 empty_env = Empty_env
@@ -25,6 +26,26 @@ extend_env x v env = Extend_env x v env
 
 extend_env_rec :: Identifier -> Identifier -> Exp -> Env -> Env
 extend_env_rec f x exp env = Extend_env_rec f x exp env
+
+extend_env_with_module :: Identifier -> TypedModule -> Env -> Env
+extend_env_with_module x mod env = Extend_env_with_module x mod env
+
+lookup_qualified_var_in_env :: Identifier -> Identifier -> Env -> ExpVal
+lookup_qualified_var_in_env mod_name var_name env = 
+  let mod = lookup_module_name_in_env mod_name env in 
+    case mod of
+      SimpleModule mod_env -> apply_env mod_env var_name
+
+lookup_module_name_in_env :: Identifier -> Env -> TypedModule
+lookup_module_name_in_env mod_name Empty_env = error (mod_name ++ " is not found.")
+lookup_module_name_in_env mod_name (Extend_env _ _ env) = lookup_module_name_in_env mod_name env
+lookup_module_name_in_env mod_name (Extend_env_rec _ _ _ env) = lookup_module_name_in_env mod_name env
+lookup_module_name_in_env mod_name (Extend_env_with_module saved_mod_name saved_mod env)
+  | mod_name==saved_mod_name = saved_mod
+  | otherwise                = lookup_module_name_in_env mod_name env
+
+-- Typed modules
+data TypedModule = SimpleModule Env
 
 -- Expressed values
 data ExpVal =
