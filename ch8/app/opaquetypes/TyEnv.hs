5f2bbe27885838ca8097592717a4ba65e9672f35
Kwanghoon Choi <lazyswamp@gmail.com>
2024-08-15 07:07:24 +0900

    change Either to Either_

--- ch8/app/opaquetypes/TyEnv.hs
+++ ch8/app/opaquetypes/TyEnv.hs
@@ -3,6 +3,7 @@
 module TyEnv where
 
 import Expr(Identifier, Type, Interface(..), Declaration(..))
+import EitherState
 
 -- Invariant: Types in type environments are always fully expanded. 
 data TyEnv = 
@@ -25,60 +26,60 @@ extend_tyenv_with_type :: Identifier -> Type -> TyEnv -> TyEnv
 extend_tyenv_with_type tname ty tyenv = 
   Extend_tyenv_with_type tname ty tyenv
 
-apply_tyenv :: TyEnv -> Identifier -> Either String Type 
-apply_tyenv Empty_tyenv var = Left $ "Variable not found: " ++ var
+apply_tyenv :: TyEnv -> Identifier -> Either_ String Type 
+apply_tyenv Empty_tyenv var = _Left $ "Variable not found: " ++ var
 apply_tyenv (Extend_tyenv v ty tyenv) var
-  | var == v = Right ty
+  | var == v = _Right ty
   | otherwise = apply_tyenv tyenv var
 apply_tyenv (Extend_tyenv_with_module _ _ tyenv) var = apply_tyenv tyenv var
 apply_tyenv (Extend_tyenv_with_type _ _ tyenv) var = apply_tyenv tyenv var
 
-lookup_qualified_var_in_tyenv :: Identifier -> Identifier -> TyEnv -> Either String Type
+lookup_qualified_var_in_tyenv :: Identifier -> Identifier -> TyEnv -> Either_ String Type
 lookup_qualified_var_in_tyenv mod_var var tyenv =
   do iface <- lookup_module_name_in_tyenv tyenv mod_var
      case iface of
        SimpleIface decls -> 
          lookup_variable_name_in_decls var decls
 
-lookup_qualified_type_in_tyenv :: Identifier -> Identifier -> TyEnv -> Either String Type
+lookup_qualified_type_in_tyenv :: Identifier -> Identifier -> TyEnv -> Either_ String Type
 lookup_qualified_type_in_tyenv mod_var tvar tyenv =
   do iface <- lookup_module_name_in_tyenv tyenv mod_var
      case iface of
        SimpleIface decls -> 
          lookup_variable_name_in_decls tvar decls  -- Exercise: Fix the case that tvar is a variable name.
 
-lookup_type_name_in_tyenv :: Identifier -> TyEnv -> Either String Type
+lookup_type_name_in_tyenv :: Identifier -> TyEnv -> Either_ String Type
 lookup_type_name_in_tyenv tname Empty_tyenv = 
-  Left $ "lookup_type_name_in_tyenv: " ++ tname ++ " not found"
+  _Left $ "lookup_type_name_in_tyenv: " ++ tname ++ " not found"
 lookup_type_name_in_tyenv tname (Extend_tyenv_with_type tname1 ty tyenv)
-  | tname == tname1 = Right ty
+  | tname == tname1 = _Right ty
   | otherwise = lookup_type_name_in_tyenv tname tyenv
 lookup_type_name_in_tyenv tname (Extend_tyenv _ _ tyenv) = 
   lookup_type_name_in_tyenv tname tyenv
 lookup_type_name_in_tyenv tname (Extend_tyenv_with_module _ _ tyenv) =
   lookup_type_name_in_tyenv tname tyenv
 
-lookup_module_name_in_tyenv :: TyEnv -> Identifier -> Either String Interface
+lookup_module_name_in_tyenv :: TyEnv -> Identifier -> Either_ String Interface
 lookup_module_name_in_tyenv Empty_tyenv mod_var = 
-  Left $ "lookup_module_name_in_tyenv: " ++ mod_var ++ " not found"
+  _Left $ "lookup_module_name_in_tyenv: " ++ mod_var ++ " not found"
 lookup_module_name_in_tyenv (Extend_tyenv _ _ tyenv) mod_var = 
   lookup_module_name_in_tyenv tyenv mod_var
 lookup_module_name_in_tyenv (Extend_tyenv_with_module m iface tyenv) mod_var
-  | m == mod_var = Right iface 
+  | m == mod_var = _Right iface 
   | otherwise = lookup_module_name_in_tyenv tyenv mod_var
 lookup_module_name_in_tyenv (Extend_tyenv_with_type _ _ tyenv) mod_var =
   lookup_module_name_in_tyenv tyenv mod_var
 
-lookup_variable_name_in_decls :: Identifier -> [Declaration] -> Either String Type
+lookup_variable_name_in_decls :: Identifier -> [Declaration] -> Either_ String Type
 lookup_variable_name_in_decls var [] = 
-  Left $ "lookup_variable_name_in_decls: " ++ var ++ " not found"
+  _Left $ "lookup_variable_name_in_decls: " ++ var ++ " not found"
 lookup_variable_name_in_decls var (ValDecl x ty : decls) 
-  | var == x = Right ty
+  | var == x = _Right ty
   | otherwise = lookup_variable_name_in_decls var decls
 lookup_variable_name_in_decls var (OpaqueTypeDecl x : decls)
-  | var == x = Left $ "lookup_variable_name_in_decls: " ++ var ++ " is an opaque type"
+  | var == x = _Left $ "lookup_variable_name_in_decls: " ++ var ++ " is an opaque type"
   | otherwise = lookup_variable_name_in_decls var decls
 lookup_variable_name_in_decls var (TransparentTypeDecl x ty : decls)
-  | var == x = Right ty
+  | var == x = _Right ty
   | otherwise = lookup_variable_name_in_decls var decls
 
--- ch8/app/opaquetypes/TypeCheck.hs
+++ ch8/app/opaquetypes/TypeCheck.hs
@@ -4,14 +4,16 @@ module TypeCheck where
 
 import Expr
 import TyEnv
+import EitherState
 
 --
 typeCheck :: Program -> IO (Either String Type)
-typeCheck program = return (type_of_program program )
+typeCheck program = 
+    return $ _run (type_of_program program)
 
 --
-add_module_defns_to_tyenv :: [ ModuleDef ] -> TyEnv -> Either String TyEnv
-add_module_defns_to_tyenv [] tyenv = Right tyenv
+add_module_defns_to_tyenv :: [ ModuleDef ] -> TyEnv -> Either_ String TyEnv
+add_module_defns_to_tyenv [] tyenv = _Right tyenv
 add_module_defns_to_tyenv (ModuleDef m iface mbody : moddefs) tyenv = 
   do actual_iface <- interface_of mbody tyenv                 -- important!
      isSubIface <- sub_iface actual_iface iface tyenv
@@ -20,36 +22,36 @@ add_module_defns_to_tyenv (ModuleDef m iface mbody : moddefs) tyenv =
         do  iface' <- expand_iface m iface tyenv               -- important!
             let newtyenv = extend_tyenv_with_module m iface' tyenv       
             add_module_defns_to_tyenv moddefs newtyenv 
-     else Left $ "In the module " ++ m
+     else _Left $ "In the module " ++ m
                    ++ "\n  expected interface: " ++ show iface
                    ++ "\n  actual interface: " ++ show actual_iface
 
-interface_of :: ModuleBody -> TyEnv -> Either String Interface 
+interface_of :: ModuleBody -> TyEnv -> Either_ String Interface 
 interface_of (ModuleBody defs) tyenv = 
   do decls <- defns_to_decls defs tyenv
-     Right $ SimpleIface decls
+     _Right $ SimpleIface decls
 
-defns_to_decls :: [ Definition ] -> TyEnv -> Either String [ Declaration ]
-defns_to_decls [] tyenv = Right []
+defns_to_decls :: [ Definition ] -> TyEnv -> Either_ String [ Declaration ]
+defns_to_decls [] tyenv = _Right []
 defns_to_decls (ValDefn var exp : defs) tyenv = 
-  case type_of exp tyenv of
-    Left errmsg -> Left $ errmsg ++ " in the declaration of " ++ var
-    Right ty -> 
-      do decls <- defns_to_decls defs (extend_tyenv var ty tyenv)
-         Right $ ValDecl var ty : decls
+  do ty <- _onLeft (type_of exp tyenv) 
+             (\errmsg -> _Left $ errmsg ++ " in the declaration of " ++ var)
+     decls <- defns_to_decls defs (extend_tyenv var ty tyenv)
+     _Right $ ValDecl var ty : decls
+
 defns_to_decls (TypeDefn var ty : defs) tyenv =
   do expanded_ty <- expand_type ty tyenv  -- expand this type
      decls <- defns_to_decls defs (extend_tyenv_with_type var expanded_ty tyenv)
-     Right $ TransparentTypeDecl var ty : decls
+     _Right $ TransparentTypeDecl var ty : decls
                  
 
-sub_iface :: Interface -> Interface -> TyEnv -> Either String Bool
+sub_iface :: Interface -> Interface -> TyEnv -> Either_ String Bool
 sub_iface (SimpleIface decls1) (SimpleIface decls2) tyenv =
   sub_decls decls1 decls2 tyenv 
 
-sub_decls :: [Declaration] -> [Declaration] -> TyEnv -> Either String Bool
-sub_decls decls1 [] tyenv = Right True 
-sub_decls [] decls2 tyenv = Right False
+sub_decls :: [Declaration] -> [Declaration] -> TyEnv -> Either_ String Bool
+sub_decls decls1 [] tyenv = _Right True 
+sub_decls [] decls2 tyenv = _Right False
 sub_decls (decl1:decls1) (decl2:decls2) tyenv =
   let name1 = name_of_decl decl1
       name2 = name_of_decl decl2
@@ -58,48 +60,46 @@ sub_decls (decl1:decls1) (decl2:decls2) tyenv =
         do bHead <- sub_decl decl1 decl2 tyenv
            tyenv' <- extend_tyenv_with_decl decl1 tyenv
            bRest <- sub_decls decls1 decls2 tyenv'
-           Right $ bHead && bRest
+           _Right $ bHead && bRest
       else 
         do tyenv' <- extend_tyenv_with_decl decl1 tyenv
            sub_decls decls1 (decl2:decls2) tyenv'
   where
     -- sub_decl is called only when x==y!
-    sub_decl :: Declaration -> Declaration -> TyEnv -> Either String Bool
+    sub_decl :: Declaration -> Declaration -> TyEnv -> Either_ String Bool
     sub_decl (ValDecl x ty1) (ValDecl y ty2) tyenv = equalExpandedType ty1 ty2 tyenv
     sub_decl (TransparentTypeDecl x ty1) (TransparentTypeDecl y ty2) tyenv = 
       equalExpandedType ty1 ty2 tyenv
-    sub_decl (TransparentTypeDecl x ty1) (OpaqueTypeDecl y) tyenv = Right True
-    sub_decl (OpaqueTypeDecl x) (OpaqueTypeDecl y) tyenv = Right True
-    sub_decl _ _ _ = Right False
+    sub_decl (TransparentTypeDecl x ty1) (OpaqueTypeDecl y) tyenv = _Right True
+    sub_decl (OpaqueTypeDecl x) (OpaqueTypeDecl y) tyenv = _Right True
+    sub_decl _ _ _ = _Right False
 
 
-extend_tyenv_with_decl :: Declaration -> TyEnv -> Either String TyEnv
-extend_tyenv_with_decl (ValDecl var ty) tyenv = Right tyenv 
+extend_tyenv_with_decl :: Declaration -> TyEnv -> Either_ String TyEnv
+extend_tyenv_with_decl (ValDecl var ty) tyenv = _Right tyenv 
 extend_tyenv_with_decl (OpaqueTypeDecl var) tyenv =
-  Right $ extend_tyenv_with_type var (TyQualified ("$m" ++ show 0) var) tyenv -- Fix this!
+  do newNum <- _get
+     _Right $ extend_tyenv_with_type var (TyQualified ("$m" ++ show newNum) var) tyenv -- Fixed!!
 extend_tyenv_with_decl (TransparentTypeDecl var ty) tyenv =
   do expanded_ty <- expand_type ty tyenv
-     Right $ extend_tyenv_with_type var expanded_ty tyenv
+     _Right $ extend_tyenv_with_type var expanded_ty tyenv
 
-equalExpandedType :: Type -> Type -> TyEnv -> Either String Bool
+equalExpandedType :: Type -> Type -> TyEnv -> Either_ String Bool
 equalExpandedType ty1 ty2 tyenv = 
   do expanded_ty1 <- expand_type ty1 tyenv
      expanded_ty2 <- expand_type ty2 tyenv
-     Right $ equalType expanded_ty1 expanded_ty2
+     _Right $ equalType expanded_ty1 expanded_ty2
 
 --
-type_of_program :: Program -> Either String Type
-type_of_program program = 
-  case program of 
-    Program moddefs modbody ->
-      case add_module_defns_to_tyenv moddefs empty_tyenv of
-        Left errMsg -> Left errMsg
-        Right tyenv -> type_of modbody tyenv
+type_of_program :: Program -> Either_ String Type
+type_of_program (Program moddefs modbody) = 
+  do tyenv <- add_module_defns_to_tyenv moddefs empty_tyenv
+     type_of modbody tyenv
     
 --
-type_of :: Exp -> TyEnv -> Either String Type
+type_of :: Exp -> TyEnv -> Either_ String Type
 
-type_of (Const_Exp n) tyenv = Right TyInt
+type_of (Const_Exp n) tyenv = _Right TyInt
 
 type_of (Var_Exp var) tyenv = apply_tyenv tyenv var
 
@@ -110,14 +110,14 @@ type_of (Diff_Exp exp1 exp2) tyenv =
      ty2 <- type_of exp2 tyenv 
      case ty1 of
        TyInt -> case ty2 of
-                 TyInt -> Right TyInt
+                 TyInt -> _Right TyInt
                  _     -> expectedButErr TyInt ty2 exp2
        _ -> expectedButErr TyInt ty1 exp1
 
 type_of (IsZero_Exp exp1) tyenv =
   do ty1 <- type_of exp1 tyenv
      case ty1 of
-       TyInt -> Right TyBool
+       TyInt -> _Right TyBool
        _     -> expectedButErr TyInt ty1 exp1
 
 type_of exp@(If_Exp exp1 exp2 exp3) tyenv =
@@ -126,7 +126,7 @@ type_of exp@(If_Exp exp1 exp2 exp3) tyenv =
      elseTy <- type_of exp3 tyenv
      case condTy of
        TyBool -> if equalType thenTy elseTy
-                 then Right thenTy
+                 then _Right thenTy
                  else inequalIfBranchTyErr thenTy elseTy exp2 exp
                       
        _      -> expectedButErr TyBool condTy exp1
@@ -134,7 +134,7 @@ type_of exp@(If_Exp exp1 exp2 exp3) tyenv =
 type_of (Let_Exp var exp1 body) tyenv =
   do expTy  <- type_of exp1 tyenv 
      bodyTy <- type_of body (extend_tyenv var expTy tyenv) 
-     Right bodyTy
+     _Right bodyTy
 
 type_of (Letrec_Exp ty proc_name bound_var bvar_ty proc_body letrec_body) tyenv =
   do expanded_funty <- expand_type (TyFun bvar_ty ty) tyenv
@@ -148,78 +148,78 @@ type_of (Letrec_Exp ty proc_name bound_var bvar_ty proc_body letrec_body) tyenv 
      letrecbodyTy <- type_of letrec_body tyenv2
      
      if equalType expanded_ty procbodyTy
-       then Right letrecbodyTy
+       then _Right letrecbodyTy
        else expectedButErr expanded_ty procbodyTy proc_body
 
 type_of (Proc_Exp var argTy body) tyenv =
   do expanded_argTy <- expand_type argTy tyenv
      bodyTy <- type_of body (extend_tyenv var expanded_argTy tyenv)
-     Right (TyFun expanded_argTy bodyTy)
+     _Right (TyFun expanded_argTy bodyTy)
 
 type_of (Call_Exp rator rand) tyenv =
   do funTy <- type_of rator tyenv
      argTy <- type_of rand tyenv
      case funTy of
        TyFun ty1 ty2 -> if equalType ty1 argTy
-                        then Right ty2
+                        then _Right ty2
                         else inequalArgtyErr ty1 argTy rator rand
        _             -> expectedFuntyButErr funTy rator
       
 -- Type expansion
-expand_type :: Type -> TyEnv -> Either String Type
-expand_type TyInt tyenv = Right TyInt
-expand_type TyBool tyenv = Right TyBool
+expand_type :: Type -> TyEnv -> Either_ String Type
+expand_type TyInt tyenv = _Right TyInt
+expand_type TyBool tyenv = _Right TyBool
 expand_type (TyFun ty1 ty2) tyenv = 
   do ty1' <- expand_type ty1 tyenv
      ty2' <- expand_type ty2 tyenv
-     Right $ TyFun ty1' ty2
+     _Right $ TyFun ty1' ty2
 expand_type (TyName n) tyenv = lookup_type_name_in_tyenv n tyenv
 expand_type (TyQualified m t) tyenv = lookup_qualified_type_in_tyenv m t tyenv
 
 -- Interface expansion
-expand_iface :: Identifier -> Interface -> TyEnv -> Either String Interface
+expand_iface :: Identifier -> Interface -> TyEnv -> Either_ String Interface
 expand_iface m iface tyenv = 
   case iface of
     SimpleIface decls -> 
       do decls' <- expand_decls m decls tyenv
-         Right $ SimpleIface decls'
+         _Right $ SimpleIface decls'
 
 -- Declaration expansion
-expand_decls :: Identifier -> [Declaration] -> TyEnv -> Either String [Declaration]
-expand_decls m [] internal_tyenv = Right []
+expand_decls :: Identifier -> [Declaration] -> TyEnv -> Either_ String [Declaration]
+expand_decls m [] internal_tyenv = _Right []
 
 expand_decls m (ValDecl x ty : decls) internal_tyenv = 
   do expanded_ty <- expand_type ty internal_tyenv
      decls' <- expand_decls m decls internal_tyenv
-     Right $ ValDecl x expanded_ty : decls'
+     _Right $ ValDecl x expanded_ty : decls'
 
 expand_decls m (OpaqueTypeDecl x : decls) internal_tyenv =
   let expanded_type = TyQualified m x                -- important!
       new_internal_env = extend_tyenv_with_type x expanded_type internal_tyenv
   in do decls' <- expand_decls m decls new_internal_env
-        Right $ TransparentTypeDecl x expanded_type : decls'
+        _Right $ TransparentTypeDecl x expanded_type : decls'
 
 expand_decls m (TransparentTypeDecl x ty : decls) internal_tyenv =
   do expanded_type <- expand_type ty internal_tyenv  -- important!
      let new_internal_env = extend_tyenv_with_type x expanded_type internal_tyenv
      decls' <- expand_decls m decls new_internal_env
-     Right $ TransparentTypeDecl x expanded_type : decls'
+     _Right $ TransparentTypeDecl x expanded_type : decls'
 
 
 -- Utilities
 expectedButErr expectedTy gotTy exp =
-  Left $ "Expected " ++ show expectedTy ++ " but got " ++ show gotTy ++ " in " ++ show exp
+  _Left $ "Expected " ++ show expectedTy ++ " but got " ++ show gotTy ++ " in " ++ show exp
 
 expectedFuntyButErr gotTy exp =
-  Left $ "Expected function type but got " ++ show gotTy ++ " in " ++ show exp
+  _Left $ "Expected function type but got " ++ show gotTy ++ " in " ++ show exp
 
 inequalIfBranchTyErr thenTy elseTy exp2 exp3 =
-  Left $ "Type mismatch: \n"
+  _Left $ "Type mismatch: \n"
           ++ "\t" ++ show thenTy ++ " in " ++ show exp2
           ++ "\t" ++ show elseTy ++ " in " ++ show exp3
 
 inequalArgtyErr argTy1 argTy2 funexp argexp =
-  Left $ "Type mismatch: \n"
+  _Left $ "Type mismatch: \n"
           ++ "\t" ++ show argTy1 ++ " for the arugment of " ++ show funexp
           ++ "\t" ++ show argTy2 ++ " in " ++ show argexp
 
