b8da8ca11459408d5ac37299adabc77d64785fec
Kwanghoon Choi <lazyswamp@gmail.com>
2024-11-14 17:31:57 +0900

    placeholders for new kinds of expression

--- ch9/app/typedoo/TypeCheck.hs
+++ ch9/app/typedoo/TypeCheck.hs
@@ -1,7 +1,10 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use camelCase" #-}
 module TypeCheck where
 
 import Expr
 import TyEnv
+import EnvStore (DenVal(SelfObject_Val))
 
 --
 typeCheck :: Program -> IO (Either String Type)
@@ -9,67 +12,78 @@ typeCheck program = return (type_of_program program )
 
 --
 type_of_program :: Program -> Either String Type
-type_of_program (Program classDecls exp) = type_of exp empty_tyenv
-    
+type_of_program (Program classDecls exp) =
+  let clzEnv = initializeStaticClassEnv classDecls
+  in  type_of clzEnv exp empty_tyenv
+
 --
-type_of :: Exp -> TyEnv -> Either String Type
+type_of :: StaticClassEnv -> Exp -> TyEnv -> Either String Type
 
-type_of (Const_Exp n) tyenv = Right TyInt
+type_of clzEnv (Const_Exp n) tyenv = Right TyInt
 
-type_of (Var_Exp var) tyenv = apply_tyenv tyenv var
+type_of clzEnv (Var_Exp var) tyenv = apply_tyenv tyenv var
 
-type_of (Diff_Exp exp1 exp2) tyenv =
-  do ty1 <- type_of exp1 tyenv 
-     ty2 <- type_of exp2 tyenv 
+type_of clzEnv (Diff_Exp exp1 exp2) tyenv =
+  do ty1 <- type_of clzEnv exp1 tyenv
+     ty2 <- type_of clzEnv exp2 tyenv
      case ty1 of
        TyInt -> case ty2 of
                  TyInt -> Right TyInt
                  _     -> expectedButErr TyInt ty2 exp2
        _ -> expectedButErr TyInt ty1 exp1
 
-type_of (IsZero_Exp exp1) tyenv =
-  do ty1 <- type_of exp1 tyenv
+type_of clzEnv (Sum_Exp exp1 exp2) tyenv =
+  do ty1 <- type_of clzEnv exp1 tyenv
+     ty2 <- type_of clzEnv exp2 tyenv
+     case ty1 of
+       TyInt -> case ty2 of
+                 TyInt -> Right TyInt
+                 _     -> expectedButErr TyInt ty2 exp2
+       _ -> expectedButErr TyInt ty1 exp1
+
+type_of clzEnv (IsZero_Exp exp1) tyenv =
+  do ty1 <- type_of clzEnv exp1 tyenv
      case ty1 of
        TyInt -> Right TyBool
        _     -> expectedButErr TyInt ty1 exp1
 
-type_of exp@(If_Exp exp1 exp2 exp3) tyenv =
-  do condTy <- type_of exp1 tyenv
-     thenTy <- type_of exp2 tyenv
-     elseTy <- type_of exp3 tyenv
+type_of clzEnv exp@(If_Exp exp1 exp2 exp3) tyenv =
+  do condTy <- type_of clzEnv exp1 tyenv
+     thenTy <- type_of clzEnv exp2 tyenv
+     elseTy <- type_of clzEnv exp3 tyenv
      case condTy of
        TyBool -> if equalType thenTy elseTy
                  then Right thenTy
                  else inequalIfBranchTyErr thenTy elseTy exp2 exp
-                      
+
        _      -> expectedButErr TyBool condTy exp1
 
-type_of (Let_Exp letBindings body) tyenv = undefined
+type_of clzEnv (Let_Exp letBindings body) tyenv = undefined
 
 -- type_of (Let_Exp var exp1 body) tyenv =
 --   do expTy  <- type_of exp1 tyenv 
 --      bodyTy <- type_of body (extend_tyenv var expTy tyenv) 
 --      Right bodyTy
 
-type_of (Letrec_Exp letrecBindings letrec_body) tyenv = undefined
+type_of clzEnv (Letrec_Exp letrecBindings letrec_body) tyenv = undefined
 
   -- do let tyenv1 = extend_tyenv bound_var bvar_ty
   --                   (extend_tyenv proc_name (TyFun bvar_ty ty) tyenv)
   --    procbodyTy <- type_of proc_body tyenv1
-     
+
   --    let tyenv2 = extend_tyenv proc_name (TyFun bvar_ty ty) tyenv
   --    letrecbodyTy <- type_of letrec_body tyenv2
-     
+
   --    if equalType ty procbodyTy
   --      then Right letrecbodyTy
   --      else expectedButErr ty procbodyTy proc_body
 
-type_of (Proc_Exp tyVarList body) tyenv = undefined
+type_of clzEnv (Proc_Exp tyVarList body) tyenv = undefined
   -- type_of (Proc_Exp var argTy body) tyenv = 
   -- do bodyTy <- type_of body (extend_tyenv var argTy tyenv)
   --    Right (TyFun argTy bodyTy)
 
-type_of (Call_Exp rator randList) tyenv = undefined
+type_of clzEnv (Call_Exp rator randList) tyenv = undefined
 -- type_of (Call_Exp rator rand) tyenv =
 --   do funTy <- type_of rator tyenv
 --      argTy <- type_of rand tyenv
@@ -79,11 +93,27 @@ type_of (Call_Exp rator randList) tyenv = undefined
 --                         else inequalArgtyErr ty1 argTy rator rand
 --        _             -> expectedFuntyButErr funTy rator
 
-type_of _ _ = undefined         
+type_of clzEnv (Block_Exp expList) tyenv = undefined
 
--- 
+type_of clzEnv (Set_Exp var exp) tyenv = undefined
+
+type_of clzEnv (List_Exp expList) tyenv = undefined
+
+type_of clzEnv (New_Object_Exp cname expList) tyenv = undefined
+
+type_of clzEnv (Method_Call_Exp exp1 mname expList) tyenv = undefined
+
+type_of clzEnv (Super_Call_Exp mname expList) tyenv = undefined
+
+type_of clzEnv Self_Exp tyenv = undefined
+
+type_of clzEnv (Cast_Exp exp cname) tyenv = undefined
+
+type_of clzEnv (InstanceOf_Exp exp cname) tyenv = undefined
+
+-- Static Class Environment
 initializeStaticClassEnv :: [ClassDecl] -> StaticClassEnv
-initializeStaticClassEnv classDecls = 
+initializeStaticClassEnv classDecls =
   AStaticClass "object" [] [] [] []
    : map classDeclToStaticClass classDecls
 
@@ -101,7 +131,7 @@ classDeclToStaticClass (Interface_Decl ifaceName methodDecls) =
 
 methodDeclToTyEnv :: MethodDecl -> (Identifier, Type)
 methodDeclToTyEnv (Method_Decl ty name tyArgs _) = (name, TyFun (map fst tyArgs) ty)
-methodDeclToTyEnv (AbstractMethod_Decl ty name tyArgs) = (name, TyFun (map fst tyArgs) ty) 
+methodDeclToTyEnv (AbstractMethod_Decl ty name tyArgs) = (name, TyFun (map fst tyArgs) ty)
 
 -- Utilities
 expectedButErr expectedTy gotTy exp =
@@ -128,5 +158,5 @@ equalType (TyFun tyList1 ty1') (TyFun tyList2 ty2') =
 equalType _ _ = False
 
 equalTypes :: [Type] -> [Type] -> Bool
-equalTypes tyList1 tyList2 = 
+equalTypes tyList1 tyList2 =
   and $ zipWith equalType tyList1 tyList2
