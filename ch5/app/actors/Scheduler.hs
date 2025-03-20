0b16d449cb0e2fff1ffc53f6c12658a80ae133be
chaendaya <jini971@naver.com>
2025-03-20 16:22:58 +0900

    fix run_next_actor'

--- ch5/app/actors/Scheduler.hs
+++ ch5/app/actors/Scheduler.hs
@@ -35,6 +35,7 @@ run_next_thread store scState actors =
           ( scState { the_ready_queue = other_ready_threads,
                       the_time_remaining = the_max_time_slice scState } ) actors )
 
+
 set_final_answer :: SchedState -> ExpVal -> SchedState
 set_final_answer scState val = scState { the_final_answer = Just val }
 
@@ -45,41 +46,21 @@ decrement_timer :: SchedState -> SchedState
 decrement_timer scState = scState { the_time_remaining = the_time_remaining scState - 1 }
 
 -- Actors
--- run_next_actor :: Store -> SchedState -> ActorState -> (FinalAnswer, Store) 
--- run_next_actor store scState (current, q, actorSpace) =
---   run_next_actor' (current, q, store, scState) actorSpace
-
--- run_next_actor' :: ActorInfo -> ActorSpace -> (FinalAnswer, Store)
--- run_next_actor' (currActor, _, _, _) (next, []) = 
---   error ("Blocking: " ++ show currActor) -- Todo: better way to handle this?
-
--- run_next_actor' currentActorInfo (next, actorList) = 
---   let actorList1 = actorList ++ [currentActorInfo]
---       (nextCurrent, nextQ, nextStore, nextScState) = head actorList1 
---       actorState1 = (nextCurrent, nextQ, (next, tail actorList1))
---   in if isempty (the_ready_queue nextScState)
---      then if null (tail actorList1)
---           then (fromJust (the_final_answer nextScState), nextStore)
---           else run_next_actor' currentActorInfo (next, tail actorList1)
---      else run_next_thread nextStore nextScState actorState1
 
 run_next_actor :: Store -> SchedState -> ActorState -> (FinalAnswer, Store) 
 run_next_actor store scState (current, q, (next, actorList)) =
   case actorList of
-    [] -> run_next_thread store scState (current, q, (next, actorList))
-    _  -> run_next_actor' (current, q, store, scState) (next, actorList)
+    [] -> run_next_thread store scState (current, q, (next, actorList))   -- If no actors are waiting, continue executing self
+    _  -> run_next_actor' (current, q, store, scState) (next, actorList)  -- If there are waiting actors, run_next_actor' with self-info
 
 run_next_actor' :: ActorInfo -> ActorSpace -> (FinalAnswer, Store)
 run_next_actor' (current, q, store, scState) (next, x:xs) =
-  if isempty (the_ready_queue scState)
-  then let (nextCurrent, nextQ, nextStore, nextScState) = x
-       in if isempty (the_ready_queue nextScState)
-          then run_next_actor nextStore nextScState (nextCurrent, nextQ, (next, xs))
-          else run_next_thread nextStore nextScState (nextCurrent, nextQ, (next, xs))
+  if isempty (the_ready_queue scState)   -- checking the current actor's ready queue
+  then let (nextCurrent, nextQ, nextStore, nextScState) = x                       -- If empty, the current actor is not added to the waiting list
+       in run_next_thread nextStore nextScState (nextCurrent, nextQ, (next, xs))  -- the next actor executes
 
-  else let actorList1 = (x:xs) ++ [(current, q, store, scState)] -- currentActorInfo
-           (nextCurrent, nextQ, nextStore, nextScState) = head actorList1
+  else let actorList1 = (x:xs) ++ [(current, q, store, scState)]                  -- If not empty, the current actor is added to the waiting list
+           (nextCurrent, nextQ, nextStore, nextScState) = head actorList1         -- the next actor executes
            actorState1 = (nextCurrent, nextQ, (next, tail actorList1))
-       in if isempty (the_ready_queue nextScState)
-          then run_next_actor nextStore nextScState actorState1
-          else run_next_thread nextStore nextScState actorState1
+       in run_next_thread nextStore nextScState actorState1
+
